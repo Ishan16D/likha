5c332eb9377442859ce8be0997d0dd1c7309a71e
Ishan <ishan16.d@gmail.com>
2023-01-17 23:21:57 -0500

    Added no backspace functionality to external js file

---
+++ app/static/app.js
@@ -0,0 +1,9 @@
+let input = document.getElementById('content');
+
+input.onkeydown = function (event) {
+
+    if (event.which == 8 || event.which == 46) {
+
+        event.preventDefault();   // turn off browser transition to the previous page 
+    }
+}; 
